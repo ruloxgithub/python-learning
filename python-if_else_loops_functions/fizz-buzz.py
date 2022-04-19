aa5f8f475f592a7e7b9f77ec694e2e75420d7f06
Raul Sandoval <raulsandoval@MacBook-Pro-de-Raul.local>
2022-04-19 17:55:14 -0600

    ejercicio extra 1

---
+++ python-if_else_loops_functions/fizz-buzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for i in range(1,101):
+        if i%3==0 and i%5==0:
+            print("fizzfuzz")
+        elif i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("fuzz")
+        else:
+            print(i)
+fizzbuzz()
