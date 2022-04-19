1df76278b3a04f0d80a2ee8751da24b710bee941
Raul Sandoval <raulsandoval@MacBook-Pro-de-Raul.local>
2022-04-18 21:11:27 -0600

    ejercicio 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+def print_last_digit(number):
+    #print(str(number))
+    #variable=new_value
+    number=str(number)
+    return number[-1]
+    # print_last_digit=""
+    # for i in number:
+    #     print_last_digit = number[-1]
+    #     print(number)
+
